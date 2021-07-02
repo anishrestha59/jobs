37a24aa5fd740291f1d13739588026dc65f33b64
Anish Shrestha <anishrestha59@gmail.com>
2021-07-02 16:11:04 +0545

    upto front end of jobs

--- src/Components/CreateJobs.js
+++ src/Components/CreateJobs.js
@@ -8,6 +8,10 @@ export default class CreateJobs extends Component {
         super()
         this.state = {
             jobname: '',
+            jobtype: '',
+            jobshift: 'day',
+            salary: '',
+            experience: '',
             description: '',
             date: new Date()
         }
@@ -23,14 +27,32 @@ export default class CreateJobs extends Component {
             jobname:event.target.value
         });
     }
-    
-    
+    changeJobType = (event) =>{
+        this.setState({
+            jobtype:event.target.value
+        });
+    } 
+
+    changeJobShift = (event) =>{
+        this.setState({
+            jobshift:event.target.value
+        });
+    }
+    changeSalary = (event) =>{
+        this.setState({
+            salary:event.target.value
+        });
+    }
+    changeExperience = (event) =>{
+        this.setState({
+            experience:event.target.value
+        });
+    }
     changeDescription(event){
         this.setState({
             description: event.target.value
         });
     }
-
 
     changeDate(Date) {
         this.setState({
@@ -38,14 +60,16 @@ export default class CreateJobs extends Component {
         });
     }
    
- 
-
     
     onSubmit(event){
         event.preventDefault();
 
         const registered = {
             jobname: this.state.jobname,
+            jobtype: this.state.jobtype,
+            jobshift: this.state.jobshift,
+            salary: this.state.salary,
+            experience: this.state.experience,
             description: this.state.description,
             date: this.state.date
         }
@@ -60,47 +84,103 @@ export default class CreateJobs extends Component {
 
 
     render() {
-        return( 
-            <div>
-                <div className= 'container'>
-                    <div className='form-div'>
-                        <form onSubmit={this.onSubmit}>
-                            <div className="form-group">
-                                <label>Jobname: </label>
-                                <input type="text"
-                                    required
-                                    className="form-control"
-                                    value={this.state.jobname}
-                                    onChange={this.changeJobname}
-                                />
-                            </div>
+        return (
+          <div>
+            <div className="container">
+              <div className="form-div">
+                <form onSubmit={this.onSubmit}>
+                  <div className="form-group">
+                    <label>Jobname: </label>
+                    <input
+                      type="text"
+                      required
+                      className="form-control"
+                      value={this.state.jobname}
+                      onChange={this.changeJobname}
+                    />
+                  </div>
 
+                  <div className="form-group">
+                    <label>Job Type: </label>
+                    <input
+                      type="text"
+                      required
+                      className="form-control"
+                      value={this.state.jobtype}
+                      onChange={this.changeJobType}
+                    />
+                  </div>
                             <div className="form-group">
-                                <label>Description: </label>
-                                <input type="text"
-                                    required
-                                    className="form-control"
-                                    value={this.state.description}
-                                    onChange={this.changeDescription}
-                                />
-                            </div>
+                                <label>Job Shift: </label>
 
-                            <div className="form-group">
-                                <label>Pick deadline: </label>
-                                <div>
-                                    <DatePicker
-                                        selected={this.state.date}
-                                        onChange={this.changeDate}
-                                    />
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio"  value="day" checked={this.state.jobshift === "day"} onChange={this.changeJobShift}  />
+                                    <label class="form-check-label"  >
+                                        Day
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio"  value="night" checked={this.state.jobshift === "night"} onChange={this.changeJobShift}  />
+                                    <label class="form-check-label" >
+                                        Night
+                                    </label>
                                 </div>
                             </div>
 
-                            <input type='submit' className='btn btn-danger btn-block' value='Create Job'/>
-                        </form>
-                    </div>
-                </div>
-            </div>
+                  <div className="form-group">
+                    <label>Salary: </label>
+                    <input
+                      type="Number"
+                      required
+                      className="form-control"
+                      value={this.state.salary}
+                      onChange={this.changeSalary}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Minimum Experience: </label>
+                    <input
+                      type="Number"
+                      required
+                      className="form-control"
+                      value={this.state.experience}
+                      onChange={this.changeExperience}
+                    />
+                  </div>
+                  <div className="text-muted mb-4">
+                            minimum experience to apply in Year
+                        </div>
 
+                  <div className="form-group">
+                    <label>Description: </label>
+                    <input
+                      type="text"
+                      required
+                      className="form-control"
+                      value={this.state.description}
+                      onChange={this.changeDescription}
+                    />
+                  </div>
+
+                  <div className="form-group">
+                    <label>Pick deadline: </label>
+                    <div>
+                      <DatePicker
+                        selected={this.state.date}
+                        onChange={this.changeDate}
+                      />
+                    </div>
+                  </div>
+
+                  <input
+                    type="submit"
+                    className="btn btn-danger btn-block"
+                    value="Create Job"
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
         );
     }
 }
